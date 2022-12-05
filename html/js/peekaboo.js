2b4adc9a5388501b2c68e856ccb4ce6d2fe8b9d9
jin <schoolyoung2m@naver.com>
2022-12-05 12:45:38 +0900

    first commit

---
+++ html/js/peekaboo.js
@@ -0,0 +1,26 @@
+// 필요한 태그 생성
+const section = document.querySelectorAll(".section");
+let secTop = [];
+
+// 스크롤 이벤트
+window.addEventListener("scroll",()=>{
+    
+    //스크롤바의 위치값 변수에 대입
+    let scTop = window.scrollY;
+
+    //각 구역의 시작위치값 반복문을 사용하여 배열에 담기
+    
+    section.forEach((item,index)=>{
+        secTop[index] = item.offsetTop - 340;
+
+        //조건문을 사용하여 해당 구간에서 요소들이 나타남 0 - 6
+        if(scTop >= secTop[index] && scTop < secTop[index + 1]){
+            section[index].classList.add("on");
+        }
+        // else if(scTop >= section[6]){
+
+        // }
+    });
+    
+
+});
